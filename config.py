cdc505dbd6e3a1918e5b98bf25c7ba184a4492d9
salesof <sofia.salejarvi@gmail.com>
2024-11-11 11:34:26 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    MAIL_SERVER = 'smtp.mailtrap.io'
+    MAIL_PORT = 2525
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = ('Microblog', 'no-reply@microblog.com')
+    ADMINS = ['admin@microblog.com']
+    POSTS_PER_PAGE = 25
